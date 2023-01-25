b3946a137c07047469fc5b3a19a936ba273b0753
evillarv <esthervillarv@gmail.com>
2023-01-24 22:46:41 -0500

    dockerfile configurtion

---
+++ src/test/java/com/nttdata/bootcamp/msconfigserver/MsServiceconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.msconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes= ConfigServerApplication.class)
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
